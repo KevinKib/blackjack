d25b7847da759d1dfe6d52fd9efd8491feacf418
Kévin Kib <sekanor21@gmail.com>
2026-02-21 21:00:48 +0100

    2.1 - Models

--- src/main/java/org/kevinkib/BlackJackService.java
+++ src/main/java/org/kevinkib/BlackJackService.java
@@ -6,8 +6,6 @@ import org.kevinkib.cards.domain.DeckType;
 import org.kevinkib.cards.domain.french.FrenchDeckFactory;
 import org.kevinkib.cards.domain.french.FrenchRank;
 import org.kevinkib.cards.domain.french.FrenchSuit;
-import org.kevinkib.config.AppConfig;
-import org.kevinkib.statistics.presentation.v1.StatisticsInternalController;
 import org.springframework.jdbc.core.JdbcTemplate;
 import com.zaxxer.hikari.HikariDataSource;
 import org.springframework.jdbc.support.GeneratedKeyHolder;
@@ -46,30 +44,101 @@ public class BlackJackService {
         this.scanner = new Scanner(System.in);
     }
 
-    public static HikariDataSource getDataSource() {
-        if (dataSource != null) {
-            return dataSource;
-        }
+    public void startGUI() {
 
-        dataSource = new HikariDataSource();
-        dataSource.setJdbcUrl(JDBC_URL);
-        dataSource.setUsername(JDBC_USERNAME);
-        dataSource.setPassword(JDBC_PASSWORD);
+        boolean stillPlay = true;
 
-        return dataSource;
+        do {
+            System.out.println();
+            System.out.println("██████╗ ██╗      █████╗  ██████╗██╗  ██╗     ██╗ █████╗  ██████╗██╗  ██╗");
+            System.out.println("██╔══██╗██║     ██╔══██╗██╔════╝██║ ██╔╝     ██║██╔══██╗██╔════╝██║ ██╔╝");
+            System.out.println("██████╔╝██║     ███████║██║     █████╔╝      ██║███████║██║     █████╔╝ ");
+            System.out.println("██╔══██╗██║     ██╔══██║██║     ██╔═██╗ ██   ██║██╔══██║██║     ██╔═██╗ ");
+            System.out.println("██████╔╝███████╗██║  ██║╚██████╗██║  ██╗╚█████╔╝██║  ██║╚██████╗██║  ██╗");
+            System.out.println("╚═════╝ ╚══════╝╚═╝  ╚═╝ ╚═════╝╚═╝  ╚═╝ ╚════╝ ╚═╝  ╚═╝ ╚═════╝╚═╝  ╚═╝");
+            System.out.println();
+
+            printStatistics();
+
+            createGame();
+
+            if (isGameOver(gameState)) {
+                printFinalState(gameState);
+                continue;
+            }
+
+            boolean running = true;
+
+            while (running) {
+                printScores();
+
+                System.out.println("\nChoose an action:");
+                System.out.println("1 - Hit");
+                System.out.println("2 - Stand");
+                System.out.print("> ");
+
+                String input = scanner.nextLine();
+
+                switch (input) {
+
+                    case "1" -> {
+                        GameState state = hit();
+                        if (isGameOver(state)) {
+                            printFinalState(state);
+                            running = false;
+                        }
+                    }
+
+                    case "2" -> {
+                        GameState state = stand();
+                        printFinalState(state);
+                        running = false;
+                    }
+
+                    default -> System.out.println("Invalid choice. Please select 1 or 2.");
+                }
+            }
+
+            System.out.println("Game over.");
+
+            boolean answered = false;
+
+            do {
+
+                System.out.println("\nKeep playing?");
+                System.out.println("1 - Yes");
+                System.out.println("2 - No");
+                System.out.print("> ");
+
+                String input = scanner.nextLine();
+
+                switch (input) {
+
+                    case "1" -> {
+                        answered = true;
+                    }
+
+                    case "2" -> {
+                        stillPlay = false;
+                        answered = true;
+                    }
+
+                    default -> System.out.println("Invalid choice. Please select 1 or 2.");
+                }
+            } while (!answered);
+        } while (stillPlay);
+
+
     }
 
-    public static HikariDataSource getTestDataSource() {
-        if (dataSource != null) {
-            return dataSource;
+    private void printStatistics() {
+        List<GameEntity> gameDBs = getGameList();
+
+        if (!gameDBs.isEmpty()) {
+            Long wonGames = gameDBs.stream().filter(gameDB -> gameDB.state().equals(GameState.WIN.name())).count();
+            double winRate = (double) wonGames / gameDBs.size() * 100;
+            System.out.println(" Win percentage : "+ winRate + "%");
         }
-
-        dataSource = new HikariDataSource();
-        dataSource.setJdbcUrl(JDBC_TEST_URL);
-        dataSource.setUsername(JDBC_USERNAME);
-        dataSource.setPassword(JDBC_PASSWORD);
-
-        return dataSource;
     }
 
     public GameState createGame() {
@@ -170,6 +239,9 @@ public class BlackJackService {
                 (rs, rowNum) -> new GameEntity(
                         rs.getLong("GAME_ID"),
                         rs.getDate("GAME_CREATION_DATE"),
+                        rs.getDate("GAME_CREATION_DATE"),
+                        rs.getLong("GAME_ID"),
+                        "Name",
                         rs.getString("GAME_STATE")
                 ));
 
@@ -259,6 +331,9 @@ public class BlackJackService {
                 (rs, rowNum) -> new GameEntity(
                         rs.getLong("GAME_ID"),
                         rs.getDate("GAME_CREATION_DATE"),
+                        rs.getDate("GAME_CREATION_DATE"),
+                        rs.getLong("GAME_ID"),
+                        "Name",
                         rs.getString("GAME_STATE")
                 ));
 
@@ -306,100 +381,6 @@ public class BlackJackService {
         gameState = GameState.from(gameDB.state());
     }
 
-    public void startGUI() {
-
-        boolean stillPlay = true;
-
-        do {
-            System.out.println();
-            System.out.println("██████╗ ██╗      █████╗  ██████╗██╗  ██╗     ██╗ █████╗  ██████╗██╗  ██╗");
-            System.out.println("██╔══██╗██║     ██╔══██╗██╔════╝██║ ██╔╝     ██║██╔══██╗██╔════╝██║ ██╔╝");
-            System.out.println("██████╔╝██║     ███████║██║     █████╔╝      ██║███████║██║     █████╔╝ ");
-            System.out.println("██╔══██╗██║     ██╔══██║██║     ██╔═██╗ ██   ██║██╔══██║██║     ██╔═██╗ ");
-            System.out.println("██████╔╝███████╗██║  ██║╚██████╗██║  ██╗╚█████╔╝██║  ██║╚██████╗██║  ██╗");
-            System.out.println("╚═════╝ ╚══════╝╚═╝  ╚═╝ ╚═════╝╚═╝  ╚═╝ ╚════╝ ╚═╝  ╚═╝ ╚═════╝╚═╝  ╚═╝");
-            System.out.println();
-
-            List<GameEntity> gameDBs = getGameList();
-            System.out.println(" Number of games played : "+gameDBs.size());
-
-            if (!gameDBs.isEmpty()) {
-                Long wonGames = gameDBs.stream().filter(gameDB -> gameDB.state().equals(GameState.WIN.name())).count();
-                double winRate = (double) wonGames / gameDBs.size() * 100;
-                System.out.println(" Win percentage : "+ winRate + "%");
-            }
-
-            createGame();
-
-            if (isGameOver(gameState)) {
-                printFinalState(gameState);
-                continue;
-            }
-
-            boolean running = true;
-
-            while (running) {
-                printScores();
-
-                System.out.println("\nChoose an action:");
-                System.out.println("1 - Hit");
-                System.out.println("2 - Stand");
-                System.out.print("> ");
-
-                String input = scanner.nextLine();
-
-                switch (input) {
-
-                    case "1" -> {
-                        GameState state = hit();
-                        if (isGameOver(state)) {
-                            printFinalState(state);
-                            running = false;
-                        }
-                    }
-
-                    case "2" -> {
-                        GameState state = stand();
-                        printFinalState(state);
-                        running = false;
-                    }
-
-                    default -> System.out.println("Invalid choice. Please select 1 or 2.");
-                }
-            }
-
-            System.out.println("Game over.");
-
-            boolean answered = false;
-
-            do {
-
-                System.out.println("\nKeep playing?");
-                System.out.println("1 - Yes");
-                System.out.println("2 - No");
-                System.out.print("> ");
-
-                String input = scanner.nextLine();
-
-                switch (input) {
-
-                    case "1" -> {
-                        answered = true;
-                    }
-
-                    case "2" -> {
-                        stillPlay = false;
-                        answered = true;
-                    }
-
-                    default -> System.out.println("Invalid choice. Please select 1 or 2.");
-                }
-            } while (!answered);
-        } while (stillPlay);
-
-
-    }
-
     private void printScores() {
         System.out.println("\n--- Current Scores ---");
         System.out.println("Player: " + getPlayerScore());
@@ -434,16 +415,30 @@ public class BlackJackService {
         }
     }
 
-    /*
+    public static HikariDataSource getDataSource() {
+        if (dataSource != null) {
+            return dataSource;
+        }
 
-    règles:
-    - un joueur, un croupier; le joueur commence
-    - le joueur pioche directement une carte
-    - un joueur a pour somme la somme des cartes (têtes à 10, as à 11)
-    - si un joueur dépasse 21, il perd
-    - sinon, le joueur le plus proche de 21 gagne
+        dataSource = new HikariDataSource();
+        dataSource.setJdbcUrl(JDBC_URL);
+        dataSource.setUsername(JDBC_USERNAME);
+        dataSource.setPassword(JDBC_PASSWORD);
 
-     */
+        return dataSource;
+    }
 
+    public static HikariDataSource getTestDataSource() {
+        if (dataSource != null) {
+            return dataSource;
+        }
+
+        dataSource = new HikariDataSource();
+        dataSource.setJdbcUrl(JDBC_TEST_URL);
+        dataSource.setUsername(JDBC_USERNAME);
+        dataSource.setPassword(JDBC_PASSWORD);
+
+        return dataSource;
+    }
 
 }
--- src/main/java/org/kevinkib/GameEntity.java
+++ src/main/java/org/kevinkib/GameEntity.java
@@ -2,5 +2,5 @@ package org.kevinkib;
 
 import java.sql.Date;
 
-public record GameEntity(Long id, Date creationDate, String state){
+public record GameEntity(Long id, Date creationDate, Date endDate, Long playerId, String playerName, String state){
 }
