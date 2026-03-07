12fc92863935df6b9137f5fa268335c4d5117462
Kévin Kib <sekanor21@gmail.com>
2026-03-07 21:17:11 +0100

    upd legacy

--- src/main/java/org/kevinkib/config/AppConfig.java
+++ src/main/java/org/kevinkib/config/AppConfig.java
@@ -2,9 +2,6 @@ package org.kevinkib.config;
 
 import org.kevinkib.LegacyBlackJackService;
 import org.kevinkib.cards.domain.french.FrenchDeckFactory;
-import org.kevinkib.statistics.business.StatisticsService;
-import org.kevinkib.statistics.business.port.out.GameRepository;
-import org.kevinkib.statistics.infrastructure.adapter.GameRepositoryH2;
 import org.springframework.boot.SpringBootConfiguration;
 import org.springframework.context.annotation.Bean;
 
