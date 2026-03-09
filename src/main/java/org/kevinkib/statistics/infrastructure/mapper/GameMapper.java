23a1c8c313add040efc79463f68b1828d80b5e66
Kévin Kib <sekanor21@gmail.com>
2026-03-09 22:03:49 +0100

    snippets

--- src/main/java/org/kevinkib/statistics/infrastructure/mapper/GameMapper.java
+++ src/main/java/org/kevinkib/statistics/infrastructure/mapper/GameMapper.java
@@ -1,27 +1,18 @@
 package org.kevinkib.statistics.infrastructure.mapper;
 
-import org.kevinkib.LegacyBlackJackService;
-import org.kevinkib.cards.domain.Hand;
 import org.kevinkib.statistics.business.model.Game;
-import org.kevinkib.statistics.business.model.GameOutcome;
 import org.kevinkib.statistics.infrastructure.entity.GameDB;
 
 import java.util.List;
-import java.util.Map;
 
 public class GameMapper {
 
-    public static Game mapToDomain(GameDB gameDB) {
-        GameOutcome outcome = GameOutcomeMapper.fromState(gameDB.state());
-
-        int playerScore = LegacyBlackJackService.calculateScore(gameDB.id());
-        int playerNbCards = LegacyBlackJackService.calculateNbCards(gameDB.id());
-
-        return new Game(outcome, playerScore, playerNbCards);
+    public static Game mapToDomain(GameDB game) {
+        return new Game(GameOutcomeMapper.fromState(game.state()));
     }
 
-    public static List<Game> mapToDomain(List<GameDB> gameDB) {
-        return gameDB.stream().map(GameMapper::mapToDomain).toList();
+    public static List<Game> mapToDomain(List<GameDB> games) {
+        return games.stream().map(GameMapper::mapToDomain).toList();
     }
 
 }
