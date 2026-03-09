010d5d4e34e5b1cfa4a4acb2896145909f5a0ecb
Kévin Kib <sekanor21@gmail.com>
2026-03-10 00:14:08 +0100

    no unused method

--- src/main/java/org/kevinkib/LegacyBlackJackService.java
+++ src/main/java/org/kevinkib/LegacyBlackJackService.java
@@ -418,6 +418,8 @@ public class LegacyBlackJackService {
 
         this.gameId = gameId;
         gameState = LegacyGameState.from(gameDB.state());
+
+        calculateScore(gameId);
     }
 
     private static List<LegacyCardEntity> getCardsFromDatabase(Long gameId) {
