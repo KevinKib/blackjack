8683ba719c3fd585dd8732866c89489075cdce08
Kévin Kib <sekanor21@gmail.com>
2026-03-09 22:54:37 +0100

    a

--- src/main/java/org/kevinkib/LegacyGameEntity.java
+++ src/main/java/org/kevinkib/LegacyGameEntity.java
@@ -2,5 +2,10 @@ package org.kevinkib;
 
 import java.sql.Date;
 
-public record LegacyGameEntity(Long id, Date creationDate, Date endDate, Long playerId, String playerName, String state){
+public record LegacyGameEntity(
+        Long id,
+        Date creationDate,
+        Date endDate, Long playerId,
+        String playerName,
+        String state) {
 }
